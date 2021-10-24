d6ea02dfc18e5259e9fca7f9531d670718eb4c56
Andrea Guffi <andreaguffi92@gmail.com>
2021-10-24 18:08:59 +0200

    finish

--- src/components/Post.js
+++ src/components/Post.js
@@ -14,8 +14,8 @@ const Post = ({id, title, body}) => {
   }
 
     return (
-        <div className='singlePost'>
-            <div className="singlePost__cont" onClick={() => toggle(id)}>
+        <div className='singlePost' onClick={() => toggle(id)}>
+            <div className="singlePost__cont">
                 <div className="circle">{id}</div>
                 <h2>{title}</h2>
             </div>
